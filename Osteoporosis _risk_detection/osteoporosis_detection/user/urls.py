44d2f8a6070bb766953993d7e0255e78ef64d2ba
Nisarga  Poojary <120793721+NisargaPoojary@users.noreply.github.com>
2024-12-11 13:02:52 +0530

    Delete Osteoporosis _risk_detection directory

--- Osteoporosis _risk_detection/osteoporosis_detection/user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.login_view, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('logout/', views.logout_view, name='logout'),
-]
