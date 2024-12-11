46004ae464def7ae7bf51ebde1ddaef9c1ae5dff
Nisarga  Poojary <120793721+NisargaPoojary@users.noreply.github.com>
2024-12-11 12:03:48 +0530

    Delete osteoporosis_detection directory

--- osteoporosis_detection/detection/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.predict_osteoporosis, name='predict_osteoporosis'),
-    path('bmi_calculator/', views.bmi_calculator, name='bmi_calculator'),
-]
